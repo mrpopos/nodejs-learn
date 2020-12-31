de7a9af35728ebac085c5d3933b9db080290bc08
tushaolong <1213167844@qq.com>
2020-12-31 10:39:33 +0800

    测试利用Ajax文件上传到node服务器

--- day20201228/upload.js
+++ day20201228/upload.js
@@ -1,17 +1,27 @@
 const http = require('http')
-const formidable = require('formidable')
+const querystring = require('querystring')
 
 const server = http.createServer((req, res) => {
   if (req.url === '/favicon.ico') {
     return
   }
+
   if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
-    const form = formidable({ multiples: true, uploadDir: './uploads'})
-    form.parse(req, (err, fields, files) => {
-      console.log(fields)
-      console.log(files)
-      res.end('upload success~~')
+    
+    var data = ''
+
+    req.on('data', function (chunk) {
+      data += chunk
     })
+
+  req.on('end', function () {
+    data = decodeURI(data)
+    console.log(data)
+
+    var dataObject = querystring.parse(data)
+    console.log(dataObject)
+  })
+    res.end(({ "statu": 200 , "ok": true }))
   }
 })
 
